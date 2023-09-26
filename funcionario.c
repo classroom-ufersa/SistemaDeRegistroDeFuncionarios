c1a51f14a7997085770edd40eebc56446522a889
gabriel <uyanelol2016@gmail.com>
2023-09-25 21:00:55 -0300

    testando

---
+++ funcionario.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcionario.h"
+
+struct funcionario
+{
+    char nome[21];
+    char cpf[21]; //usar union?
+    char cargo[21];
+    char setor[21];
+    float salario;
+    char dataContratacao[21];
+    int jornadaTrabalho;
+    struct funcionario *proximo;
+    struct funcionario *anterior;
+}funcionario;
+
+int TrataNome(char nome[21])
+{
+    int i, TamanhoNome = strlen(nome);
+  
+    //Verifica se o nome é muito pequeno
+    if(TamanhoNome <= 2){
+        printf("Nome muito pequeno\n");
+        return 1;
+    }
+    
+    //Verifica se o nome contém caracteres não alfanuméricos nem espaços em branco
+    for(i = 0; nome[i] != '\0'; i++){
+        if(!isalnum(nome[i]) && nome[i] != ' '){
+            printf("Entrada Inválida\n");
+            return 1;
+        }
+    }
+    
+    //Converte o nome para maiúsculas
+    for(i = 0; nome[i] != '\0'; i++){
+        nome[i] = toupper(nome[i]);
+    }
+    
+    return 0;
+}
+
+//Funcionario *criaLista(void)
+
+//void insereLista(Funcionario *ponteiroLista, char *nome)
+
+//Funcionario *retiraLista(Funcionario *ponteiroLista, char *nome)
