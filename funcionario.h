e45efb5b3af611606eaca8f3c12bbac40a7878bf
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-09-27 17:08:07 -0300

    Update funcionario.h

--- funcionario.h
+++ funcionario.h
@@ -3,3 +3,7 @@ typedef struct funcionario Funcionario;
 int trataNome(char nome[21]);
 
 Funcionario *insereFuncionario(Funcionario *ponteiroLista, char *nome, char *documento, char *setor, float salario, char *data, char *jornada);
+
+Funcionario*BuscaFuncionario(Funcionario*ponteiroLista,char*nome);
+
+void imprimeFuncionarios(Funcionario *ponteiroLista);
