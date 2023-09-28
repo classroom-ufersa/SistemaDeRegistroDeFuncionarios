1f8e810acbbec3c2c5402a4cf2a269946bc9d381
gabriel <uyanelol2016@gmail.com>
2023-09-28 07:34:26 -0300

    teste

--- funcionario.h
+++
@@ -1,5 +0,0 @@
-typedef struct funcionario Funcionario;
-
-int trataNome(char nome[21]);
-
-Funcionario *insereFuncionario(Funcionario *ponteiroLista, char *nome, char *documento, char *setor, float salario, char *data, char *jornada)
